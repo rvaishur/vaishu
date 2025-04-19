59a4ced9598a4286b0a575c8594c777de03b479a
rvaishur <vaishuvaish0706@gmail.com>
2025-04-19 12:42:08 +0530

    1st upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function add() {
+
+    var a= 5;
+    var b= 10;
+    var c= a+b;
+    console.log('${c}');
+    
+}
+
+add()
+function checkEvenO(number){
+
+if (number % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+
+}
+
+var button = document.querySelector('button');
+var box = document.getElementById('box');
+
+function changeColor(){
+    let number = 7;
+
+    if (number % 2 === 0) {
+      console.log("Even");
+    } else {
+      console.log("Odd");
+    }
+function changeColor(){
+if(box.style.background=='red')
+{
+    box.style.background=='yellow';
+}
+else{
+    box.style.background="red"
+}
+}
